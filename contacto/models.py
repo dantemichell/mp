6545d9cb6a748ca39bfdf5a04b17659a820ef7bf
myexoplanet <dantemichell@gmail.com>
2018-10-16 11:03:42 -0300

    Cambiado charfield a emailfield

--- contacto/models.py
+++ contacto/models.py
@@ -12,7 +12,7 @@ class Contacto(models.Model):
 	run = models.CharField(primary_key=True, max_length=50)
 	nombre = models.CharField(max_length=50)
 	apellido = models.CharField(max_length=50)
-	correo = models.CharField(max_length=50)
+	correo = models.EmailField(max_length=50)
 	fecha_nacimiento = models.DateTimeField(blank=True, null=False)
 	telefono = models.IntegerField()
 	region = models.CharField(max_length=50)
